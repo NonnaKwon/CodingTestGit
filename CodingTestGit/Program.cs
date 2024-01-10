88f7197fbc0de32d99cafb6a7ad2b26997ff362a
ChanGyu Yu <dbcksrb99@gmail.com>
2024-01-10 16:17:38 +0900

    feat : 캐릭터의 좌표

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120845 풀었음! - 유찬규");
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120808 풀었다! - 유찬규");
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120922 풀었다! - 유찬규");
+            Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120861 풀었다! - 유찬규");
         }
     }
 }
