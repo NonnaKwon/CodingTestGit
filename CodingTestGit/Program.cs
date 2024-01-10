b181365e63eeef4475aa8ff18551d24674ab546a
Saerom <non1233@naver.com>
2024-01-10 15:17:29 +0900

    가장 큰 수 찾기

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/181847?language=csharp 풀엇음 - 권새롬");
-
+            Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120899?language=csharp 풀음 - 권새롬");
         }
     }
 }
