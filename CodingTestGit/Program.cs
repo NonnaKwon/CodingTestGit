bbef42da72bcad7c5c60c083bb42e76d8de9ab63
YJ <cvcv_cv@naver.com>
2024-01-10 16:11:11 +0900

    chore : 메인 수정

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -4,19 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120819 아이스 아메리카노 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120826 특정 문제 제거하기 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120585 머쓱이보다 키 큰 사람- 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120809 배열 두 배 만들기 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120824 짝수 홀수 개수 - 유정");
 
-            Console.WriteLine("K의 갯수 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120887");
-            Console.WriteLine("대문자와 소문자 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120893");
-            Console.WriteLine("가위바위보 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120839");
-            Console.WriteLine("개미군단 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120837");
-            Console.WriteLine("가장 큰 수 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120899");
-            Console.WriteLine("직각삼각형 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120823");
-            Console.WriteLine("문자열정렬하기(1) https://school.programmers.co.kr/learn/courses/30/lessons/120850")
-              
-            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120845풀었음! - 유찬규");
-            Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120808 풀었다! - 유찬규");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120822 문자열 뒤집기 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120811 중앙값 구하기 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120813 짝수는 싫어요 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120818 옷가게 할인 받기 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120908 문자열안에 문자열 - 유정");
 
-        }
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120889 삼각형의 완성조건 (1) - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120849 모음 제거 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120841 점의 위치 구하기 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120583 중복된 숫자 개수 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120825 문자 반복 출력하기 - 유정");
+
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120910 세균 증식 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120847 최댓값 만들기 (1) - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120909 제곱수 판별하기 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120836 순서쌍의 개수 - 유정");
+			Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120903 배열의 유사도 - 유정");
+		}
     }
 }
