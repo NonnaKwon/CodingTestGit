d70b4981bf82737d7b9ea8f83699a266bf91ce16
Park Jun Hung <matte1428@naver.com>
2024-01-10 15:32:33 +0900

    feat:외계행성의나이
    외계행성에 떨어진 머쓱이

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -12,7 +12,8 @@
             Console.WriteLine("개미군단 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120837");
             Console.WriteLine("가장 큰 수 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120899");
             Console.WriteLine("직각삼각형 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120823");
-            Console.WriteLine("문자열정렬하기(1) https://school.programmers.co.kr/learn/courses/30/lessons/120850")
+            Console.WriteLine("문자열정렬하기(1) https://school.programmers.co.kr/learn/courses/30/lessons/120850");
+            Console.WriteLine("외계행성의나이 https://school.programmers.co.kr/learn/courses/30/lessons/120834");
         }
     }
 }
