4270990028441c8cc0165b6464ee4a4eb440b6d8
JongMin <qkjm4836@gmail.com>
2024-01-10 15:54:23 +0900

    feat : 몫 구하기

---
+++ CodingTestGit/JM_sol (4).cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int answer = 0;
+        int a = num1 / num2;
+        return a;
+        return answer;
+    }
+}
