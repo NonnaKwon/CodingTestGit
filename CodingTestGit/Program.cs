450b42c5a8e6b5b630a1503d3aa3ad0ca4465230
chulwoo byun <bcw1992@naver.com>
2024-01-10 15:17:36 +0900

    자릿수 더하기 메인주소 누락된거

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120845풀었음! - 유찬규");
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120808 풀었다! - 유찬규");
+            Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120906 - 변철우");
         }
     }
 }
