4b67327fa02032fdf091542340dd5bac72b2a99d
JongMin <qkjm4836@gmail.com>
2024-01-10 15:54:03 +0900

    feat : 나이 출력

---
+++ CodingTestGit/JM_sol (2).cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int age)
+    {
+        int answer = 0;
+        int a = 2022;
+        int b = 2023 - age;
+        return b;
+        return answer;
+    }
+}
