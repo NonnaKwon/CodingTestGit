e373bdb9fb904f4cb19358b2484b68cf661c8ed1
chulwoo byun <bcw1992@naver.com>
2024-01-10 16:50:23 +0900

    test : 배열 자르기

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120808 풀었다! - 유찬규");
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120906 - 변철우");
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120851 - 변철우");
+            Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120833# - 변철우");
         }
     }
 }
---
+++ CodingTestGit/bcw_sol3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTestGit
+{
+    //배열 자르기
+    internal class bcw_sol3
+    {
+        public int[] solution(int[] numbers, int num1, int num2)
+        {
+            int length = 0;
+            int j = 0;
+            if (num1 != 0)
+            {
+                length = num2 - num1 + 1;
+            }
+            else
+            {
+                length = num2 + 1;
+            }
+            int[] answer = new int[length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i >= num1 && num2 >= i)
+                {
+                    answer[j] = numbers[i];
+                    j++;
+                }
+            }
+            return answer;
+        }
+    }
+}
