5e19e82b280f9e6226839e273998695dc00aa659
ChanGyu Yu <dbcksrb99@gmail.com>
2024-01-10 15:46:18 +0900

    feat : 종이 자르기

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -4,8 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120845풀었음! - 유찬규");
+            Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120845 풀었음! - 유찬규");
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120808 풀었다! - 유찬규");
+            Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120922 풀었다! - 유찬규");
         }
     }
 }
