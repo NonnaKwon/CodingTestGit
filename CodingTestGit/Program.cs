f7b78e6285b35cf00ca157f13067357bf8a694ce
Park Jun Hung <matte1428@naver.com>
2024-01-10 15:50:20 +0900

    feat:문자열정렬하기2

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -12,11 +12,12 @@
             Console.WriteLine("개미군단 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120837");
             Console.WriteLine("가장 큰 수 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120899");
             Console.WriteLine("직각삼각형 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120823");
-            Console.WriteLine("문자열정렬하기(1) https://school.programmers.co.kr/learn/courses/30/lessons/120850");
+            Console.WriteLine("문자열정렬하기(1) 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120850");
             Console.WriteLine("외계행성의나이 https://school.programmers.co.kr/learn/courses/30/lessons/120834");
             Console.WriteLine("피자나눠먹기(2) https://school.programmers.co.kr/learn/courses/30/lessons/120815");
             Console.WriteLine("숫자찾기 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120904");
             Console.WriteLine("369게임 풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120891");
+            Console.WriteLine("문자열정렬하기(2)풀었음 https://school.programmers.co.kr/learn/courses/30/lessons/120911");
         }
     }
 }
