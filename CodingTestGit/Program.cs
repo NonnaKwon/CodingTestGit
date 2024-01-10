b6fe307f07575b4ee9f6ad31668002e7de3e0b0d
ChanGyu Yu <dbcksrb99@gmail.com>
2024-01-10 16:57:16 +0900

    feat : 치킨쿠폰 치킨치킨

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120808 풀었다! - 유찬규");
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120922 풀었다! - 유찬규");
             Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120861 풀었다! - 유찬규");
+            Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120884 풀었다! 치킨! - 유찬규");
         }
     }
 }
