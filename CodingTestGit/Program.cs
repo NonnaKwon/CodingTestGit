d5fa8235f29e35f5ecd2cfa38356b3bd70ac0934
ChanGyu Yu <dbcksrb99@gmail.com>
2024-01-10 14:41:12 +0900

    feat : 주사위의 개수 문제 해결

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.Write("주사위의 개수 문제 풀었음. - 유찬규\n");
+            Console.WriteLine(" 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120845");
         }
     }
 }
