b0d0bca0693e8e1abc3905be3ba3d5368a827e21
YJ <cvcv_cv@naver.com>
2024-01-10 14:55:25 +0900

    배열 두배 만들기

---
+++ CodingTestGit/YJ_sol2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class YJ_sol2
+{
+	// 배열 두배 만들기
+	public int[] solution(int[] numbers)
+	{
+		int[] answer = new int[] { };
+		int len = numbers.Length;
+		for (int i = 0; i < len; i++)
+		{
+			numbers[i] = numbers[i] * 2;
+		}
+		return numbers;
+	}
+}
