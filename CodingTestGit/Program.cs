96da50db9aebe2c8812ebb02fc4fc8a388b796b4
Saerom <non1233@naver.com>
2024-01-10 15:12:21 +0900

    0떼기 문제

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/181847?language=csharp 풀엇음 - 권새롬");
+
         }
     }
 }
