18551f358a2e5c15dd14b22f7bc37062c689a2af
JongMin <qkjm4836@gmail.com>
2024-01-10 15:55:51 +0900

    feat : 코테 주소

--- CodingTestGit/Program.cs
+++ CodingTestGit/Program.cs
@@ -4,8 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120845풀었음! - 유찬규");
-            Console.WriteLine("https://school.programmers.co.kr/learn/courses/30/lessons/120808 풀었다! - 유찬규");
-        }
+            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120803 두 수의 차 - 김종민(1)");
+            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120820 나이 출력 - 김종민(2)");
+            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120804 두 수의 곱 - 김종민(3)");
+            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120805 몫 구하기 - 김종민(4)");
+            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120810 나머지 구하기 - 김종민(5)");
+            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120807 숫자 비교하기 - 김종민(6)");
+            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120802 두 수의 합 - 김종민(7)");
+            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120806 두 수의 나눗셈 - 김종민(8)");
+            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120829 각도기 - 김종민(9)");
+            Console.Write("https://school.programmers.co.kr/learn/courses/30/lessons/120831 짝수의 합 - 김종민(10)");
+            Console.Write("");
+            Console.Write("");
+
+        }   
     }
 }
